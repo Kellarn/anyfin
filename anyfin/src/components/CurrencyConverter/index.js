8d83c726ac181b1f2695817b0f1b3938a9254f95
Sebastian <sebastian.kallstedt@codebet.se>
2020-05-10 21:19:06 +0200

    Updated in axios param and added searchAmount to differentiate between search amount and searched amount

--- anyfin/src/components/CurrencyConverter/index.js
+++ anyfin/src/components/CurrencyConverter/index.js
@@ -7,6 +7,7 @@ const CurrencyConverter = currencyCode => {
   const [submit, setSubmit] = useState(false);
   const [currentAmount, setCurrentAmount] = useState(0);
   const [convertedAmount, setConvertedAmount] = useState(null);
+  const [searchedAmount, setSearchedAmount] = useState(null);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState(false);
 
@@ -16,10 +17,17 @@ const CurrencyConverter = currencyCode => {
       try {
         let data = await axios({
           method: 'get',
-          url: `http://data.fixer.io/api/convert?access_key=${process.env.REACT_APP_API_ACCESS_KEY}&from=SEK&to=${currencyCode.currencyCode}&amount=${currentAmount}`,
-          mode: 'cors'
+          url: `http://data.fixer.io/api/convert`,
+          mode: 'cors',
+          params: {
+            access_key: process.env.REACT_APP_API_ACCESS_KEY,
+            from: 'SEK',
+            to: currencyCode.currencyCode,
+            amount: currentAmount
+          }
         });
         if (data.status === 200) {
+          setSearchedAmount(currentAmount);
           setConvertedAmount(data.data.result);
         }
         setSubmit(false);
@@ -56,7 +64,7 @@ const CurrencyConverter = currencyCode => {
       {convertedAmount && (
         <div className='result'>
           <p>
-            {currentAmount} SEK to {currencyCode.currencyCode} ={' '}
+            {searchedAmount} SEK to {currencyCode.currencyCode} ={' '}
             {convertedAmount}
           </p>
         </div>
